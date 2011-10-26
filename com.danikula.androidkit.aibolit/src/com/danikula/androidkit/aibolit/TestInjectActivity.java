b40416a67ab4fa6a11835fb83ffa3effccaca310
danikula <danikula@2144ecab-a3d8-49e3-9d7b-c8936c1bbffb>
2011-10-26 10:28:26 +0000

    add androidkit.aibolit project
    git-svn-id: http://xp-dev.com/svn/androidcore/trunk@13 2144ecab-a3d8-49e3-9d7b-c8936c1bbffb

---
+++ com.danikula.androidkit.aibolit/src/com/danikula/androidkit/aibolit/TestInjectActivity.java
@@ -0,0 +1,15 @@
+package com.danikula.androidkit.aibolit;
+
+import com.danikula.androidkit.aibolit.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestInjectActivity extends Activity {
+    
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
