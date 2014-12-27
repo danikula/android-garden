36f3b4889ece67432aca9b9c279d315a1b9f3b7e
Alexey Danilov <danikula@gmail.com>
2014-12-28 01:08:39 +0300

    add useful methods to ViewHolder

--- library/src/main/java/com/danikula/android/garden/ui/list/ViewHolder.java
+++ library/src/main/java/com/danikula/android/garden/ui/list/ViewHolder.java
@@ -1,83 +1,99 @@
 package com.danikula.android.garden.ui.list;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkState;
-
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.SparseArray;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.danikula.android.garden.utils.UiUtils;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+
 public class ViewHolder {
 
-	private View parent;
+    private View parent;
 
-	private SparseArray<View> views;
+    private SparseArray<View> views;
 
-	@Deprecated
-	// for backward compatibility
-	public ViewHolder(View parent, int... ids) {
-		this(parent);
-	}
+    @Deprecated
+    // for backward compatibility
+    public ViewHolder(View parent, int... ids) {
+        this(parent);
+    }
 
-	public ViewHolder(View parent) {
-		this.views = new SparseArray<View>();
-		this.parent = checkNotNull(parent, "Root view can't be null!");
-	}
+    public ViewHolder(View parent) {
+        this.views = new SparseArray<View>();
+        this.parent = checkNotNull(parent, "Root view can't be null!");
+    }
 
-	public View getParent() {
-		return parent;
-	}
+    public View getParent() {
+        return parent;
+    }
 
-	public Context getContext() {
-		return parent.getContext();
-	}
+    public Context getContext() {
+        return parent.getContext();
+    }
 
-	public <V extends View> V findView(int id) {
-		View view = views.get(id);
-		if (view == null) {
-			view = parent.findViewById(id);
-			checkNotNull(view, "View vith id '0x%s' not exist!", Integer.toHexString(id));
-			views.put(id, view);
-		}
-		return (V) view;
-	}
+    public <V extends View> V findView(int id) {
+        View view = views.get(id);
+        if (view == null) {
+            view = parent.findViewById(id);
+            checkNotNull(view, "View vith id '0x%s' not exist!", Integer.toHexString(id));
+            views.put(id, view);
+        }
+        return (V) view;
+    }
 
-	public TextView findTextView(int textViewId) {
-		return (TextView) findView(textViewId);
-	}
+    public TextView findTextView(int textViewId) {
+        return (TextView) findView(textViewId);
+    }
 
-	public ImageView findImageView(int imageViewId) {
-		return (ImageView) findView(imageViewId);
-	}
+    public ImageView findImageView(int imageViewId) {
+        return (ImageView) findView(imageViewId);
+    }
 
-	public void setText(int textViewId, CharSequence text) {
-		TextView textView = findViewAndCheckType(textViewId, TextView.class);
-		textView.setText(text);
-	}
+    public void setText(int textViewId, CharSequence text) {
+        TextView textView = findViewAndCheckType(textViewId, TextView.class);
+        textView.setText(text);
+    }
 
-	public void setShowParentContextMenuOnClickListener(int viewId) {
-		View view = findViewAndCheckType(viewId, View.class);
-		view.setOnClickListener(new PerformLongClickOnClickListener(parent));
-	}
+    public void setShowParentContextMenuOnClickListener(int viewId) {
+        View view = findViewAndCheckType(viewId, View.class);
+        view.setOnClickListener(new PerformLongClickOnClickListener(parent));
+    }
 
-	public void resetTextViews(int... textViewIds) {
-		for (int textViewId : textViewIds) {
-			findTextView(textViewId).setText("");
-		}
-	}
+    public void resetTextViews(int... textViewIds) {
+        for (int textViewId : textViewIds) {
+            findTextView(textViewId).setText("");
+        }
+    }
 
-	public void resetImageViews(int... imageViewIds) {
-		for (int imageViewId : imageViewIds) {
-			findImageView(imageViewId).setImageBitmap(null);
-		}
-	}
+    public void resetImageViews(int... imageViewIds) {
+        for (int imageViewId : imageViewIds) {
+            findImageView(imageViewId).setImageBitmap(null);
+        }
+    }
 
-	private <V extends View> V findViewAndCheckType(int viewId, Class<? extends View> classView) {
-		V view = findView(viewId);
-		checkState(classView.isInstance(view), "View with id '%s' is not %s", viewId, classView.getSimpleName());
-		return view;
-	}
+    public void setVisibility(boolean visible, int viewId) {
+        View view = findView(viewId);
+        UiUtils.setVisibility(visible, view);
+    }
+
+    public void hideIfEmpty(int... textViewIds) {
+        for (int textViewId : textViewIds) {
+            TextView view = findTextView(textViewId);
+            boolean empty = TextUtils.isEmpty(view.getText());
+            UiUtils.setVisibilityGone(!empty, view);
+        }
+    }
+
+    private <V extends View> V findViewAndCheckType(int viewId, Class<? extends View> classView) {
+        V view = findView(viewId);
+        checkState(classView.isInstance(view), "View with id '%s' is not %s", viewId, classView.getSimpleName());
+        return view;
+    }
 
 }
