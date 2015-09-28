669fe6948d206f83d94ccaa479d94aafaa575dcb
Alexey Danilov <danikula@gmail.com>
2015-09-28 11:58:57 +0600

    process service's arguments less strict

--- library/src/main/java/com/danikula/android/garden/task/RequestExecutorService.java
+++ library/src/main/java/com/danikula/android/garden/task/RequestExecutorService.java
@@ -1,10 +1,5 @@
 package com.danikula.android.garden.task;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import static com.google.common.base.Preconditions.checkArgument;
-
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -12,29 +7,32 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Process;
 import android.os.ResultReceiver;
+import android.util.Log;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class RequestExecutorService extends Service {
 
+    private static final String LOG_TAG = "RequestExecutorService";
     private static final String EXTRA_REQUEST = "com.danikula.android.garden.task.ExtraCommand";
-
     private static final String EXTRA_RESULT_RECEIVER = "com.danikula.android.garden.task.ExtraResultReceiver";
 
     private ExecutorService executor = Executors.newFixedThreadPool(5);
-
     private Handler uiThreadHandler;
-    
+
     static void start(Context context, Request request, ResultReceiver callback) {
         Intent intent = new Intent(context, RequestExecutorService.class);
         intent.putExtra(RequestExecutorService.EXTRA_REQUEST, request);
         intent.putExtra(RequestExecutorService.EXTRA_RESULT_RECEIVER, callback);
         context.startService(intent);
     }
-    
+
     static void stop(Context context) {
-        Intent intent = new Intent(context, RequestExecutorService.class);        
+        Intent intent = new Intent(context, RequestExecutorService.class);
         context.stopService(intent);
     }
-    
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -43,8 +41,15 @@ public class RequestExecutorService extends Service {
 
     @Override
     public int onStartCommand(final Intent intent, int flags, int startId) {
-        checkArgument(intent.hasExtra(EXTRA_REQUEST), "There is no command in extras");
-        checkArgument(intent.hasExtra(EXTRA_RESULT_RECEIVER), "There is no result receiver in extras");
+        if (!intent.hasExtra(EXTRA_REQUEST)) {
+            Log.e(LOG_TAG, "There is no command in extras");
+            return START_NOT_STICKY;
+        }
+
+        if (!intent.hasExtra(EXTRA_RESULT_RECEIVER)) {
+            Log.e(LOG_TAG, "There is no result receiver in extras");
+            return START_NOT_STICKY;
+        }
 
         Request command = (Request) intent.getSerializableExtra(EXTRA_REQUEST);
         ResultReceiver receiver = intent.getParcelableExtra(EXTRA_RESULT_RECEIVER);
